d5050d6e508695a39a6c1f23993666ac805e82e3
Abishek Phuyal <abishek2040@proton.me>
2024-02-13 16:48:31 +0000

    Added List comprehension example

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -1,2 +1,11 @@
 # List Comprehension is a short way of creating a list using a for loop in Python. It is a concise way to create a list in Python.
 
+numbers = [1, 2, 3, 4, 5]
+
+def multiply(amount):
+    return amount * 10
+
+# Here, we use list comprehension to create a list of numbers and call the list "result"
+
+result = [multiply(number) for number in numbers]
+print(f"Here's the new list of numbers: {result}")
