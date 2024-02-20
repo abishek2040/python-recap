5362273cd145db08acec8a40b511d85b92900da3
Abishek Phuyal <abishek2040@proton.me>
2024-02-20 22:28:28 +0000

    Importing modules in python

---
+++ importing.py
@@ -0,0 +1,9 @@
+# When you create a program or a project, it will contain multiple code files and it will be very long, so to manage the code. 
+# We need to import the different modules i.e. the different parts of the project broken down into smaller chunks. 
+# They can be imported directly to the main executable file. 
+# Let's see an example of how we can import modules to the main python file. 
+
+from name import nam
+
+user1 = nam("Wayne", "Rooney")
+user1.print_name()
---
+++ name.py
@@ -0,0 +1,11 @@
+# This is a module called name.py, here we input our name and surname and gets printed in a well formatted way. 
+
+class nam:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+    
+    def print_name(self):
+        print(f"Hello, my name is {self.name} {self.surname}! ")
+
+
